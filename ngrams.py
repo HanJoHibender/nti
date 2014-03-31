6593caaf58b747b10b5a13be84c0a43ca9c593ae
Elise van der Pol <koster.elise@gmail.com>
2014-03-31 12:42:20 +0200

    command line args almost done

--- ngrams.py
+++ ngrams.py
@@ -1,35 +1,50 @@
+#!/usr/bin/python
+import sys
+import argparse
 from collections import Counter
-n=2
+
+n=3
 m=4
 ngrams = []
 position = 0;
-currentGram = ""
+textFile="train.txt"
+
+parser = argparse.ArgumentParser(description="Get ngrams")
+parser.add_argument('-corpus', metavar = 'textFile', type=str, help="flag for corpus")
+parser.add_argument('-n', metavar='n', type=int, help="flag for ngram")
+parser.add_argument('-m', metavar='m', type=int, help="flag for top m")
+args = parser.parse_args()
+print vars(args)
+
+nArg = vars(args)['n']
+mArg = vars(args)['m']
+textArg = vars(args)['corpus']
+
+if(nArg != "None"):
+	n = nArg
+else:
+	n = 3
+
+if(mArg != "None"):
+	m = mArg
+else:
+	m = 3
+
+if(textArg != "None"):
+	textFile = textArg
+else:
+	textFile = "train.txt"
+
 f = open("train.txt", "r")
 lines = f.readlines()
 text = ""
-print lines
 for i in lines:
 	text += i
-print text
 
 wordArray = text.split()
-print wordArray
 for i in range(0, len(wordArray)):
 	ngrams.append(" ".join(wordArray[position:position+n]))
-	
 	position+=1
-#with open("train.txt") as f:
-#	while True:
-#		f.seek(position)
-#		c = f.read(n)
-#		if not c:
-#			print "End of file"
-#			break
-#		position+=1
-#		ngrams.append(c)
-print ngrams
 countDict = Counter(ngrams)
-#print countDict['Of']
-#print countDict['of']
-#print countDict['OF']
 print countDict.most_common(m)
+
